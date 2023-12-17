05c99266bd1f05625316d43ebf85893060d2a284
Niels-vdb <nielsvdbosch95@gmail.com>
2023-12-17 20:18:31 +0100

    UPDATE: Added detail list to detail page

--- movies/views.py
+++ movies/views.py
@@ -8,5 +8,7 @@ def index(request):
     movies = Movie.objects.all()
     return render(request, "movies/index.html", {"movies": movies})
 
+
 def detail(request, movie_id):
-    return HttpResponse(movie_id)
+    movie = Movie.objects.get(pk=movie_id)
+    return render(request, "movies/detail.html", {"movie": movie})
